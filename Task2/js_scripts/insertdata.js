63e4f2e0ec478e516686185e736f721029baef29
folorunsho jacob <folorunshojacob05@gmail.com>
2023-05-30 07:02:03 +0100

    Final commit

--- Task2/js_scripts/insertdata.js
+++ Task2/js_scripts/insertdata.js
@@ -1,15 +1,16 @@
+// Ajax API that iserts data into the ddatabase
 $(document).ready(function() {
-  $("#courseregistration").submit(function(e) {
+  $("#courseregistration").submit(function(e) { // targets the button with id "courseregistration"
     e.preventDefault();
-    $.ajax({
-      url: "savedata.php",
-      method: "POST",
+    $.ajax({ 
+      url: "savedata.php",// specifies ithe script (target url)
+      method: "POST",// specifies the method type (POST/GET)
       data: $(this).serialize(),
-      success: function(response) {
+      success: function(response) { //displays a message to the user upon success of the function
         alert("You Have Successfully Added a New Course," + '<br>' + "Thank You!");
         $("#courseregistration")[0].reset();
       },
-      error: function(xhr, status, error) {
+      error: function(xhr, status, error) {// display the error message the POST method is unsuccessful
         alert("Registration failed: " + error);
       }
     });
@@ -17,9 +18,10 @@ $(document).ready(function() {
 
 });
 
+// function to control checking and unchecking of all checkboxes on the report page.
 function toggleAll(source) {
   var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-  checkboxes.forEach(function(checkbox) {
+  checkboxes.forEach(function(checkbox) {// looping through all checkboxes
       checkbox.checked = source.checked;
   });
 }
--- Task2/validate_login.php
+++ Task2/validate_login.php
@@ -2,17 +2,17 @@
 
 require 'conn.php';
 
-if(isset($_POST['uname']) && isset($_POST['psw'])){
-    $uname = $_POST['uname'];
-    $psw = $_POST['psw'];
+if(isset($_POST['uname']) && isset($_POST['psw'])){// condition that checks the type of method been performed on the form
+    $uname = $_POST['uname'];// sets the username to variable name "uname"
+    $psw = $_POST['psw'];// sets password to variable name "psw"
 
-    if(!empty($uname) && !empty($psw)){
+    if(!empty($uname) && !empty($psw)){// checks if user entered username and password
 
-        $sql = "select * from users where username = '$uname' and password = '$psw'";
-        $result = $conn->query($sql);
+        $sql = "select * from users where username = '$uname' and password = '$psw'";// query to check if a user with the username and password exist
+        $result = $conn->query($sql);// query execution
 
-        if($result->num_rows > 0){
-            header('location:home.php');
+        if($result->num_rows > 0){//checking if it returns any row that matches the username and password of the user
+            header('location:home.php');// if it matches any row then   access will be granted to the user
             exit;
         }
         else{
